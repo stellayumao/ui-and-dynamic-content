a0126a0c3a49a85bdff75c0d97f798964ee0453e
Yumao Chen <46456153+stellayumao@users.noreply.github.com>
2019-03-11 08:10:46 -0400

    index.js created

---
+++ js/index.js
@@ -0,0 +1,26 @@
+//References to active elements
+const userInput = document.getElementById("user-input");
+const btn = document.getElementById("trigger-click");
+const displayData = document.getElementById("display-data");
+const err = document.querySelector(".error");
+
+function addItem() {
+	//Default value of error paragraph
+	err.textContent = null;
+	//Removing Whitespace
+	userInput.value = userInput.value.trim()
+	//If user input is not empty
+	if (userInput.value !== "") {
+		//Create a list item
+		let listItem = document.createElement("li");
+		//assign the value to the list item
+		listItem.textContent = "> " + userInput.value;
+		//append the new element to DOM
+		displayData.appendChild(listItem);
+	} else {
+		err.textContent = "Nothing entered!";
+	}
+}
+
+//Register button for
+btn.addEventListener("click",addItem);
